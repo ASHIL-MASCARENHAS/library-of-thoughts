3255def73f627695243b5d56078269db2a540827
ASHIL-MASCARENHAS <ashilmascarenhas@gmail.com>
2025-07-05 02:06:11 +0530

    added views path #6

--- server.js
+++ server.js
@@ -52,6 +52,7 @@ const authAdmin = admin.auth();
 // Express app setup
 const app = express();
 app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
