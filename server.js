6940f4a2f8c06809094f89206dda394fba3ce6f1
ASHIL-MASCARENHAS <ashilmascarenhas@gmail.com>
2025-07-05 00:59:03 +0530

    Add vercel.json for deployment and update start script

--- server.js
+++ server.js
@@ -53,6 +53,7 @@ const authAdmin = admin.auth(); // Get Firebase Admin Auth instance
 // Express app setup
 const app = express();
 app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
